ff871b6501d79cae6789eaf0f27713ee23f97079
Bhaskar <bhaskar9.koilada@gmail.com>
2021-06-30 14:25:26 +0530

    todolist

---
+++ frontEnd/Answer-03/todoapp/src/List.js
@@ -0,0 +1,11 @@
+const movies = ['abc', 'def' , 'fjb']
+const movieNames = movies.map((movie) => {
+    return <li>{movie}</li>
+})
+
+function List() {
+    
+    <movieNames/>  
+}
+
+export default List;
---
+++ frontEnd/Answer-03/todoapp/src/Todo.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./App.css";
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}> Complete</button>
+        <button onClick={() => removeTodo(index)}> X </button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <br></br><br></br>
+      <h3 className='btn'>Press Enter to Add</h3>
+    </form>
+  );
+}
+
+function Todos() {
+  const [todos, setTodos] = React.useState([]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+          <h1> To-Do List </h1>
+      
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <br></br><br></br>
+        <TodoForm addTodo={addTodo} />
+      
+      </div>
+    </div>
+  );
+}
+
+export default Todos;
